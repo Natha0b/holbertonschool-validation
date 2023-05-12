68f03acc1feccf2219f9ffba76bbacb95d92b8f7
Natha0b <laura.barona04@gmail.com>
2023-05-12 18:28:45 -0500

    add changes in the module 3 task 3

--- module3_task3/setup.sh
+++ module3_task3/setup.sh
@@ -1,5 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+# Install appropriate version of hugo to be run in docker container 'ubuntu 18.04'
 apt-get update && apt-get install -y make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-dpkg -i hugo_extended_0.109.0_linux-amd64.deb
-rm -f hugo_extended_0.109.0_linux-amd64.deb
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
+mv hugo /usr/local/bin/
+rm hugo_extended_0.109.0_Linux-64bit.tar.gz
+
+# Install markdownlint and zip tools
+apt-get install zip -y
+npm install -g markdownlint-cli -y
+
+# Generate a Go-Hugo website
+make build
