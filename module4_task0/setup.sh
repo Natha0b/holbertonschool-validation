c17f79956b0e09a7a8d6225005de0f3073fb8c78
Natha0b <laura.barona04@gmail.com>
2023-05-17 19:40:26 -0500

    add information in the content post

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -3,3 +3,5 @@ apt-get update && apt-get install -y make wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 dpkg -i hugo_extended_0.109.0_linux-amd64.deb
 rm -f hugo_extended_0.109.0_linux-amd64.deb
+npm install -g markdown-link-check
+npm install -g markdownlint-cli
